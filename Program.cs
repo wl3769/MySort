c996c6b48187acfc9fbe87d0e78da42765f62246
wl3769 <56995455+wl3769@users.noreply.github.com>
2019-10-25 20:57:51 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            int[] arr = {23, 44, 66, 76, 98, 11, 3, 9, 7};
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("排序后的数组：");
+            foreach (int item in arr)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
